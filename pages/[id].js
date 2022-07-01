a586e9b4fa1fe47953b57c64009d5ffe030a1c1d
Mikhail <mixail.workmail@yandex.ru>
2022-07-01 16:23:22 +0300

    refactor: add comments for blocks

--- pages/[id].js
+++ pages/[id].js
@@ -52,7 +52,7 @@ function PostPage({ trendingResults, followResults, providers }) {
     [db, id]
   );
 
-  if (!session) return <Login providers={prividers} />;
+  if (!session) return <Login providers={providers} />;
 
   return (
     <div className="">
@@ -63,7 +63,10 @@ function PostPage({ trendingResults, followResults, providers }) {
 
       <main className="bg-black min-h-screen flex max-w-[1500px] mx-auto">
         <Sidebar />
+
+        {/* post with comments */}
         <div className="flex-grow border-l border-r border-gray-700 max-w-2xl sm:ml-[73px] xl:ml-[370px]">
+          {/* header */}
           <div className="flex items-center px-1.5 py-2 border-b border-gray-700 text-[#d9d9d9] font-semibold text-xl gap-x-4 sticky top-0 z-50 bg-black">
             <div
               className="hoverAnimation w-9 h-9 flex items-center justify-center xl:px-0"
@@ -73,8 +76,10 @@ function PostPage({ trendingResults, followResults, providers }) {
             </div>
             Tweet
           </div>
+          {/* header end */}
 
           <Post id={id} post={post} postPage />
+          {/* comments */}
           {comments.length > 0 && (
             <div className="pb-72">
               {comments.map((comment) => (
@@ -86,7 +91,10 @@ function PostPage({ trendingResults, followResults, providers }) {
               ))}
             </div>
           )}
+          {/* comments end */}
         </div>
+        {/* post with comments end */}
+
         {/* Widgets */}
         {isOpen && <Modal />}
       </main>
