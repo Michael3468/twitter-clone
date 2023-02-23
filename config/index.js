2d57a1b263c446bf4b523cb63894fb4075af16ac
Mikhail <mixail.workmail@yandex.ru>
2023-02-23 17:27:00 +0300

    refactor: add link 2

--- config/index.js
+++ config/index.js
@@ -3,4 +3,4 @@ const dev = process.env.NODE_ENV !== 'production';
 console.log(dev);
 
 // export const server = dev ? 'http://localhost:3000' : 'https://twitter-clone-rhccoder.vercel.app/';
-export const server = 'https://twitter-clone-rhccoder.vercel.app/';
+export const server = 'https://twitter-clone-git-bugfix-versel-deploy-rhccoder.vercel.app/';
