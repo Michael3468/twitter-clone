f9c379f1a92292ca3c4cd97b3354b0349a32af91
Mikhail <mixail.workmail@yandex.ru>
2023-03-01 14:11:20 +0300

    feat([...nextauth].js): add CredentialsProvider

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -3,24 +3,45 @@ import GoogleProvider from 'next-auth/providers/google';
 import GitHubProvider from 'next-auth/providers/github';
 
 export default NextAuth({
-  providers: [
-    GoogleProvider({
-      clientId: process.env.GOOGLE_CLIENT_ID,
-      clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-    }),
+  site: process.env.NEXTAUTH_URL,
 
-    GitHubProvider({
-      clientId: process.env.GITHUB_ID,
-      clientSecret: process.env.GITHUB_SECRET,
-    }),
+  providers: [
+    process.env.VERCEL_ENV === "preview"
+      ? CredentialsProvider({
+          name: "Credentials",
+          credentials: {
+            username: {
+              label: "Username",
+              type: "text",
+              placeholder: "jsmith",
+            },
+            password: { label: "Password", type: "password" },
+          },
+          async authorize() {
+            return {
+              id: 1,
+              name: "J Smith",
+              email: "jsmith@example.com",
+              image: "https://i.pravatar.cc/150?u=jsmith@example.com",
+            }
+          },
+        })
+      :
+
+      GoogleProvider({
+        clientId: process.env.GOOGLE_CLIENT_ID,
+        clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+      }),
+
+      GitHubProvider({
+        clientId: process.env.GITHUB_ID,
+        clientSecret: process.env.GITHUB_SECRET,
+      }),
   ],
 
   callbacks: {
     async session({ session, token }) {
-      session.user.tag = session.user.name
-        .split(' ')
-        .join('')
-        .toLocaleLowerCase();
+      session.user.tag = session.user.name.split(' ').join('').toLocaleLowerCase();
 
       session.user.uid = token.sub;
       return session;
